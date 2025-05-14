b268b7629bf06eaf34e5bd33c1e2352dada9ee03
Berke Güleryüz <berkeguleryuz@gmail.com>
2025-05-14 18:02:14 +0200

    v0

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <About />
+      <section className="z-0 min-h-screen bg-blue-500" />
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,16 +4,19 @@ import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
 
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
   const totalVideos = 4;
-  const nextVdRef = useRef(null);
+  const nextVdRef = useRef<HTMLVideoElement>(null);
 
   const handleVideoLoad = () => {
     setLoadedVideos((prev) => prev + 1);
@@ -42,7 +45,7 @@ const Hero = () => {
           height: "100%",
           duration: 1,
           ease: "power1.inOut",
-          onStart: () => nextVdRef.current.play(),
+          onStart: () => void nextVdRef.current?.play(),
         });
         gsap.from("#current-video", {
           transformOrigin: "center center",
@@ -60,14 +63,36 @@ const Hero = () => {
 
   useGSAP(() => {
     gsap.set("#video-frame", {
-      
-    })
-  })
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      borderRadius: "0 0 40% 10%",
+    });
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
 
   const getVideoSrc = (index: number) => `videos/hero-${index}.mp4`;
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+      {loading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
