680acaf8a672de91d34961c4931b9cb69d2613a3
Berke Güleryüz <berkeguleryuz@gmail.com>
2025-05-14 18:56:23 +0200

    v0

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-"use client"
+"use client";
 
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
-      <section className="z-0 min-h-screen bg-blue-500" />
     </main>
   );
 }
