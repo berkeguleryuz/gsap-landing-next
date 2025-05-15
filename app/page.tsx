fe71e1e5b44caacba227760c7b6c1a2b0bc38ea9
Berke Güleryüz <berkeguleryuz@gmail.com>
2025-05-15 14:27:11 +0200

    v0

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import About from "@/components/About";
+import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 }
